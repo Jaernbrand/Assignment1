bba1d1ea962a145862c15cef10e728e5ba9016e9
Gouvernator <tomassandberg86@hotmail.com>
2014-11-25 15:53:58 +0100

    changes made

--- myObject.js
+++ myObject.js
@@ -1,6 +1,19 @@
 var myObject = {
+	var parents = [ ]; 
+	
 	create: function(prototypeList){
-
+	parents = [];
+	// Om prototypeList är en array och den inte är null eller tom
+	if(Array.isArray(prototypeList) && (prototypeList != null || prototypeList.length == 0)){
+			for(i = 0; i < prototypeList.length; i++){
+				// Om objekt och inte array
+				if(typeof prototypeList[i] === 'object' && !Array.isArray(prototypeList[i])){
+					parents.push(prototypeList[i]);
+				}
+			}
+		}
+	}	
+	return object.create(this);	
 	},
 
 	call: function(funcName, parameters){
