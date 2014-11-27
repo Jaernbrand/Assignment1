b9e7d01c31451513f8f1330a19209f1701f474bc
Gouvernator <tomassandberg86@hotmail.com>
2014-11-27 11:37:49 +0100

    createclass kommentarer

--- createClass.js
+++ createClass.js
@@ -6,6 +6,7 @@ function createClass(className, superClassList){
 	parents : [] 
 	};
 
+	//Kollar att superClassList är en array med objekt och att objekten i arrayen inte är "array-objekt", läggs sedan i parents
 	if (Array.isArray(superClassList) && (superClassList != null || superClassList.length == 0)){
     	for(var i = 0; i < superClassList.length; i++){
         	if (typeof superClassList[i] === 'object' && !Array.isArray(superClassList[i])){
@@ -14,8 +15,10 @@ function createClass(className, superClassList){
     	}
 	}
 
+	//new()
 	newClass.new = function(){ return Object.create(this); };	
 
+	//haveFunction()
 	newClass.haveFunction = 
 	function (funcName, parameters){
 				this.foundFunction = false;
@@ -24,44 +27,39 @@ function createClass(className, superClassList){
 				}
 	};
 	
-	//---------------------------------------------------------------------------------------------------------------------------------------------------------
-	//---------------------------------------------------------------------------------------------------------------------------------------------------------
-	
 	//call()
 	newClass.call =
 	function(funcName, parameters) {
 		
-		if(this.haveFunction(funcName, parameters)){ //Kolla om Rotobjektet har funktionen
+		if(this.haveFunction(funcName, parameters)){ //Kolla om rotobjektet har funktionen
 			var f = this[funcName];
 			return f.apply(null, parameters); 
 		}
 		
-		// Rotobjektet läggs i visited(alla vi besökt) och i arrayen currentLevel, leveln vi är på..
-		var visited = [ this]; 
-		var currentLevel = [this]; //array med objekt vars parents vi ska söka igenom
+		// Rotobjektet läggs i visited (alla vi besökt) och i arrayen currentLevel (leveln vi är på)
+		var visited = [this];  //De vi besökt och inte vill besöka igen
+		var currentLevel = [this]; //Array med objekt vars parents vi ska söka igenom
 		
 		var c = 0;
 		while(currentLevel.length != 0){ //While currentLevel's not empty
 			nextLevel = [ ];
 			
 			for(var k = 0; k < currentLevel.length; ++k){
-					for(var m = 0; m < currentLevel[k].parents.length; ++m){
-						if (visited.indexOf(currentLevel[k].parents[m]) == -1){ //om vi inte har vi sätt den förut " ! "
+					for(var m = 0; m < currentLevel[k].parents.length; ++m){ 
+						if (visited.indexOf(currentLevel[k].parents[m]) == -1){ //Om vi inte har vi sätt den förut
 							if(currentLevel[k].parents[m].haveFunction(funcName, parameters)){ //Kolla om den har funktionen	
 								var f = currentLevel[k].parents[m][funcName];
-								return f.apply(null, parameters); //Hittar funktionen och returnerar den..
+								return f.apply(null, parameters); //Hittar funktionen och returnerar den
 							}
-							nextLevel.push(currentLevel[k].parents[m]); //Föräldrarna som ska kollas nästa varv..
-							visited.push(currentLevel[k].parents[m]);
+							nextLevel.push(currentLevel[k].parents[m]); //Föräldrarna som ska kollas nästa varv
+							visited.push(currentLevel[k].parents[m]); //Läggs till som besökt
 						}
 					}
 			}					
-			currentLevel = nextLevel; //Fel om det kopieras...vilket det förmodligen görs..
+			currentLevel = nextLevel;
 		}
 	}; // call				
-	
-	//---------------------------------------------------------------------------------------------------------------------------------------------------------
-	//---------------------------------------------------------------------------------------------------------------------------------------------------------			
+			
 	return newClass;	
 }
 
