5722f7885408162b3a22c6ab1d22ce0c8ea85ba5
Jaernbrand <henrikjarnbrand@gmail.com>
2014-11-27 09:03:35 +0100

    createClass versionen med en lång lista som itereras över. Alt nr 2.

--- createClass.js
+++ createClass.js
@@ -6,14 +6,14 @@ function createClass(className, superClassList){
 	parents : superClassList
 	};
 
-	newClass.new = 
-	function(){ return Object.create(this); };
+	newClass.new = function(){ return Object.create(this); };
 	
-	newClass.call = 
-	function(funcName, parameters) {
-		
+	newClass.call = function(funcName, parameters) {
 		function existsAndCall(object, funcName, parameters){
 			object.foundFunction = false;
+
+			document.write("KODEN HÄR KÖRS!!!<br>");
+
 			if (funcName in object && typeof object[funcName] === "function" ){
 				object.foundFunction = true;
 				//return this[funcName](parameters);
@@ -22,6 +22,21 @@ function createClass(className, superClassList){
 			}
 		}
 		
+		this.foundFunction = false;
+		var returnValue = existsAndCall(this, funcName, parameters);
+
+		var allParents = this.parents;
+		document.write("allParents before loop: " + allParents + "<br>");
+		for (var k=0; k < allParents.length && this.foundFunction === true; ++k){
+			//allParents.concat(this.parents[k].parents);
+			allParents.push(allParents[k].parents);
+			
+			returnValue = existsAndCall(allParents[k], funcName, parameters);
+			this.foundFunction = allParents[k].foundFunction;
+		}
+		document.write("allParents after loop: " + allParents + "<br>");
+		
+	/*		
 		for (var i=0; i < this.parents.length; ++i){
 			var returnValue = existsAndCall(parents[i], funcName, parameters);
 			this.foundFunction = this.parents[i].foundFunction;
@@ -29,11 +44,12 @@ function createClass(className, superClassList){
 				return returnValue;
 			}
 		}
+	*/	
+		return returnValue;
 		
-		
-	}
+	}; // call
 	
-	
+	return newClass;	
 }
 
 
