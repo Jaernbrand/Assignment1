a0243642b8b3eec42a19a50b54beeecc8bfc9403
Gouvernator <tomassandberg86@hotmail.com>
2014-11-27 09:21:00 +0100

    såja

--- createClass.js
+++ createClass.js
@@ -8,7 +8,7 @@ function createClass(className, superClassList){
 
 	newClass.new = function(){ return Object.create(this); };
 	
-<<<<<<< HEAD
+
 	newClass.haveFunction = 
 	function (funcName, parameters){
 				this.foundFunction = false;
@@ -54,48 +54,6 @@ function createClass(className, superClassList){
 	
 	//---------------------------------------------------------------------------------------------------------------------------------------------------------
 	//---------------------------------------------------------------------------------------------------------------------------------------------------------			
-=======
-	newClass.call = function(funcName, parameters) {
-		function existsAndCall(object, funcName, parameters){
-			object.foundFunction = false;
-
-			document.write("KODEN HÄR KÖRS!!!<br>");
-
-			if (funcName in object && typeof object[funcName] === "function" ){
-				object.foundFunction = true;
-				//return this[funcName](parameters);
-				var f = object[funcName];
-				return f.apply(null, parameters);
-			}
-		}
-		
-		this.foundFunction = false;
-		var returnValue = existsAndCall(this, funcName, parameters);
-
-		var allParents = this.parents;
-		document.write("allParents before loop: " + allParents + "<br>");
-		for (var k=0; k < allParents.length && this.foundFunction === true; ++k){
-			//allParents.concat(this.parents[k].parents);
-			allParents.push(allParents[k].parents);
-			
-			returnValue = existsAndCall(allParents[k], funcName, parameters);
-			this.foundFunction = allParents[k].foundFunction;
-		}
-		document.write("allParents after loop: " + allParents + "<br>");
-		
-	/*		
-		for (var i=0; i < this.parents.length; ++i){
-			var returnValue = existsAndCall(parents[i], funcName, parameters);
-			this.foundFunction = this.parents[i].foundFunction;
-			if (this.foundFunction){
-				return returnValue;
-			}
-		}
-	*/	
-		return returnValue;
-		
-	}; // call
->>>>>>> origin/master
 	
 	return newClass;	
 }
